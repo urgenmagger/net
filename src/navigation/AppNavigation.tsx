6149823b5aa00a99519269e3b8948448f19fc95a
urgenmagger <evgen.magger@gmail.com>
2023-12-21 09:43:17 +0300

    Add ts config

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {RootStackList, Screens} from './types';
-import {Details} from '../screens/details/Details';
-import {MainScreen} from '../screens/main/MainScreen';
+import { RootStackList, Screens } from './types';
+import { Details } from '../screens/details/Details';
+import { MainScreen } from '../screens/main/MainScreen';
 
 const Stack = createNativeStackNavigator<RootStackList>();
 
@@ -17,12 +17,12 @@ export const AppNavigation = () => {
           <Stack.Screen
             name={Screens.Root}
             component={MainScreen}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name={Screens.Details}
             component={Details}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
         </>
       </Stack.Navigator>
